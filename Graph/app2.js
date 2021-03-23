387399706d9915c79ae975916a5486353326c605
jack griffin <jgriffin1314@ail.com>
2021-03-22 21:51:42 -0400

    jack pushing

--- Graph/app2.js
+++ Graph/app2.js
@@ -14,84 +14,89 @@ var svg = d3.select("#my_dataviz")
           "translate(" + margin.left + "," + margin.top + ")");
 
 //Read the data
-d3.csv("../Data/minwage.csv", function(data) {
+d3.csv("../Data/minwage_forgraph.csv").then(function(data) {
 
-    data.forEach(function(d) {
-      d.Year = +d.Year;
-      d.State = d.State;
-      d.Effective.Minimum.Wage = +d.Effective.Minimum.Wage;
-    });
+  console.log(data);
 
-    // List of groups (here I have one group per column)
-    var allGroup = ["Alabama", "Alaska", "Arizona"]
+});
 
-    // add the options to the button
-    d3.select("#selectButton")
-      .selectAll('myOptions')
-     	.data(allGroup)
-      .enter()
-    	.append('option')
-      .text(function (d) { return d; }) // text showed in the menu
-      .attr("value", function (d) { return d; }) // corresponding value returned by the button
 
-    // A color scale: one color for each group
-    var myColor = d3.scaleOrdinal()
-      .domain(allGroup)
-      .range(d3.schemeSet2);
+//     data.forEach(function(d) {
+//       d.Year = +d.Year;
+//       d.State = d.State;
+//       d.Effective.Minimum.Wage = +d.Effective.Minimum.Wage;
+//     });
 
-    // Add X axis --> it is a date format
-    var x = d3.scaleLinear()
-      .domain([1968,2020])
-      .range([ 0, width ]);
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+//     // List of groups (here I have one group per column)
+//     var allGroup = ["Alabama", "Alaska", "Arizona"]
 
-    // Add Y axis
-    var y = d3.scaleLinear()
-      .domain( [0,20])
-      .range([ height, 0 ]);
-    svg.append("g")
-      .call(d3.axisLeft(y));
+//     // add the options to the button
+//     d3.select("#selectButton")
+//       .selectAll('myOptions')
+//      	.data(allGroup)
+//       .enter()
+//     	.append('option')
+//       .text(function (d) { return d; }) // text showed in the menu
+//       .attr("value", function (d) { return d; }) // corresponding value returned by the button
 
-    // Initialize line with group a
-    var line = svg
-      .append('g')
-      .append("path")
-        .datum(data)
-        .attr("d", d3.line()
-          .x(function(d) { return x(+d.Year) })
-          .y(function(d) { return y(+d.Effective.Minimum.Wage) })
-        )
-        .attr("stroke", function(d){ return myColor("Alabama") })
-        .style("stroke-width", 4)
-        .style("fill", "none")
+//     // A color scale: one color for each group
+//     var myColor = d3.scaleOrdinal()
+//       .domain(allGroup)
+//       .range(d3.schemeSet2);
 
-    // A function that update the chart
-    function update(selectedGroup) {
+//     // Add X axis --> it is a date format
+//     var x = d3.scaleLinear()
+//       .domain([1968,2020])
+//       .range([ 0, width ]);
+//     svg.append("g")
+//       .attr("transform", "translate(0," + height + ")")
+//       .call(d3.axisBottom(x));
 
-      // Create new data with the selection?
-      var dataFilter = data.map(function(d){return {Year: d.Year, value:d[selectedGroup]} })
+//     // Add Y axis
+//     var y = d3.scaleLinear()
+//       .domain( [0,20])
+//       .range([ height, 0 ]);
+//     svg.append("g")
+//       .call(d3.axisLeft(y));
 
-      // Give these new data to update line
-      line
-          .datum(dataFilter)
-          .transition()
-          .duration(1000)
-          .attr("d", d3.line()
-            .x(function(d) { return x(+d.Year) })
-            .y(function(d) { return y(+d.value) })
-          )
-          .attr("stroke", function(d){ return myColor(selectedGroup) })
-    }
+//     // Initialize line with group a
+//     var line = svg
+//       .append('g')
+//       .append("path")
+//         .datum(data)
+//         .attr("d", d3.line()
+//           .x(function(d) { return x(+d.Year) })
+//           .y(function(d) { return y(+d.Effective.Minimum.Wage) })
+//         )
+//         .attr("stroke", function(d){ return myColor("Alabama") })
+//         .style("stroke-width", 4)
+//         .style("fill", "none")
 
-    // When the button is changed, run the updateChart function
-    d3.select("#selectButton").on("change", function(d) {
-        // recover the option that has been chosen
-        var selectedOption = d3.select(this).property("value")
-        // run the updateChart function with this selected option
-        update(selectedOption)
-    })
+//     // A function that update the chart
+//     function update(selectedGroup) {
 
-})
+//       // Create new data with the selection?
+//       var dataFilter = data.map(function(d){return {Year: d.Year, value:d[selectedGroup]} })
+
+//       // Give these new data to update line
+//       line
+//           .datum(dataFilter)
+//           .transition()
+//           .duration(1000)
+//           .attr("d", d3.line()
+//             .x(function(d) { return x(+d.Year) })
+//             .y(function(d) { return y(+d.value) })
+//           )
+//           .attr("stroke", function(d){ return myColor(selectedGroup) })
+//     }
+
+//     // When the button is changed, run the updateChart function
+//     d3.select("#selectButton").on("change", function(d) {
+//         // recover the option that has been chosen
+//         var selectedOption = d3.select(this).property("value")
+//         // run the updateChart function with this selected option
+//         update(selectedOption)
+//     })
+
+// })
 
