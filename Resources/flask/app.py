0313cbef6b3d83ba9d223b693ea7f8e03f730e5d
Manuel Mejia <bxprdr25@hotmail.com>
2021-03-28 00:08:22 -0400

    more changes to .html file

---
+++ Resources/flask/app.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify, render_template
+
+# 2. Create an app, being sure to pass __name__
+app = Flask(__name__)
+
+# 3. Define what to do when a user hits the index route
+@app.route("/")
+def home():
+    return render_template("project-2-test.html", text="A Historical Examination Throughout the Decades by State") 
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
